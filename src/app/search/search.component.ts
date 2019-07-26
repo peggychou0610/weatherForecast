c60060d88c63ffcafba19592aca3dc7d5cbb4834
peggychou0610 <peggychou0610@gmail.com>
2019-07-26 17:10:16 +0800

    Add files via upload

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -9,20 +9,25 @@ import { Component, OnInit } from '@angular/core';
           <th colspan="2" style="text-align: left;">查詢條件</th>
         </tr>
         <tr>
-          <td>地區：<input type="text" placeholder="Search for city..."></td>
+          <td>搜尋：<input (keyup)="onKey($event)" type="text" placeholder="Search for..."></td>
           <td>日期：<input type="date"></td>
         </tr>
       </table>
-      <app-weather></app-weather>
+      <app-weather [childMessage]="parentMessage"></app-weather>
     </div>
   `,
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+  parentMessage: string;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onKey(event) {
+    this.parentMessage = event.target.value;
   }
 
 }
