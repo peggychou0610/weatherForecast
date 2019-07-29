ce58f00c683bac6e83566d95b01eb41d821607ef
peggychou0610 <peggychou0610@gmail.com>
2019-07-29 16:58:03 +0800

    Add files via upload

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,17 +1,43 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 import { WeatherService, ResultsItem } from './weather.service';
-import { SortTablePipe } from '../sort-table.pipe';
 
 @Component({
   selector: 'app-weather',
-  templateUrl: './weather.component.html',
+  template: `
+    <table class="weeklyWeather">
+      <tr>
+        <th colspan="8" style="text-align: left; cursor: auto;">一週天氣</th>
+      </tr>
+      <tr>
+        <th (click)="sortBy(0)">ID</th>
+        <th>地區</th>
+        <th>天氣狀況</th>
+        <th (click)="sortBy(3)">最高溫</th>
+        <th (click)="sortBy(4)">最低溫</th>
+        <th (click)="sortBy(5)">parameterValue1</th>
+        <th>開始時間</th>
+        <th>結束時間</th>
+      </tr>
+      <!--<ng-container *ngIf="weather.length > 0">-->
+        <tr *ngFor="let data of weather | filter: childMessage">
+          <td style="text-align: center;">{{ data?._id }}</td>
+          <td style="text-align: center;">{{ data?.locationName }}</td>
+          <td>{{ data?.parameterName1 }}</td>
+          <td>{{ data?.parameterName2 }}°{{ data?.parameterUnit2 }}</td>
+          <td>{{ data?.parameterName3 }}°{{ data?.parameterUnit3 }}</td>
+          <td>{{ data?.parameterValue1 }}</td>
+          <td>{{ data?.startTime | date: 'y-MM-dd, HH:mm:ss' }}</td>
+          <td>{{ data?.endTime | date: 'y-MM-dd, HH:mm:ss' }}</td>
+        </tr>
+      <!-- </ng-container> -->
+    </table>
+  `,
   styleUrls: ['./weather.component.css']
 })
 export class WeatherComponent implements OnInit {
   @Input() childMessage: string;
   weather: Array<ResultsItem>;
-  pipe: SortTablePipe;
 
   constructor(private service: WeatherService) { }
 
@@ -26,6 +52,6 @@ export class WeatherComponent implements OnInit {
   }
 
   sortBy(key) {
-    this.weather = this.pipe.transform(this.weather, key);
+    this.weather = this.service.doSort(this.weather, key);
   }
 }
--- src/app/weather/weather.service.ts
+++ src/app/weather/weather.service.ts
@@ -15,6 +15,66 @@ export class WeatherService {
     return this.http.get<RootObject>(this.getUrl);
   }
 
+  doSort(weather: Array<ResultsItem>, key: number) {
+    let switching: boolean, i: number, x, y, shouldSwitch: boolean, dir: string, switchcount = 0;
+    switching = true;
+    dir = 'asc';
+    while (switching) {
+      switching = false;
+      for (i = 0; i < (weather.length - 1); i++) {
+        shouldSwitch = false;
+        switch (key) {
+          case 0:
+            x = weather[i]._id;
+            y = weather[i + 1]._id;
+            break;
+          case 3:
+            x = weather[i].parameterName2;
+            y = weather[i + 1].parameterName2;
+            break;
+          case 4:
+            x = weather[i].parameterName3;
+            y = weather[i + 1].parameterName3;
+            break;
+          case 5:
+            x = weather[i].parameterValue1;
+            y = weather[i + 1].parameterValue1;
+            break;
+        }
+        if (dir === 'asc') {
+          if (Number(x) > Number(y)) {
+            shouldSwitch = true;
+            break;
+          }
+        } else if (dir === 'desc') {
+          if (Number(x) < Number(y)) {
+            shouldSwitch = true;
+            break;
+          }
+        }
+      }
+      if (shouldSwitch) {
+        weather = this.doSwitch(i, weather);
+        switching = true;
+        switchcount ++;
+      } else {
+        if (switchcount === 0 && dir === 'asc') {
+          dir = 'desc';
+          switching = true;
+        }
+      }
+    }
+    return weather;
+  }
+
+  doSwitch(i: number, weather: Array<ResultsItem>) {
+    let temp;
+    temp = weather[i];
+    weather[i] = weather[i + 1];
+    weather[i + 1] = temp;
+    return weather;
+  }
+
 }
 
 export interface RootObject {
