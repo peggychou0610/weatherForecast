bebcd635f8a0b9586484110d0adaaf3e5c98c4ea
peggychou0610 <peggychou0610@gmail.com>
2019-08-02 09:38:50 +0800

    Add files via upload

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -6,29 +6,49 @@ import { Component, OnInit } from '@angular/core';
     <div>
       <table class="search">
         <tr>
-          <th colspan="2" style="text-align: left;">查詢條件</th>
+          <th colspan="9" style="text-align: left;">查詢條件</th>
         </tr>
         <tr>
           <td>地區：<input (keyup)="onKey($event)" type="text" placeholder="Search for..."></td>
-          <td>日期：<input type="date"></td>
+          <td>日期：</td>
+          <td *ngFor="let date of arr">
+            <input type="checkbox" (checked)="checkDate()" name="{{ date }}" value="{{ date }}" checked>{{ date }}
+          </td>
         </tr>
       </table>
-      <app-weather [childMessage]="parentMessage"></app-weather>
+      <app-weather [childMessage]="parentMessage" [childDate]="parentDate"></app-weather>
     </div>
   `,
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
   parentMessage: string;
+  parentDate: string;
+  arr = [];
 
   constructor() { }
 
   ngOnInit() {
+    this.getDates();
   }
 
   onKey(event) {
     // childMessage: weather component
     this.parentMessage = event.target.value;
+  }
+
+  checkDate() {
+    // childDate: weather component
+    this.parentDate = 'checked';
+  }
+
+  getDates() {
+    const today = new Date();
+    for (var i = 0; i < 7; i++) {
+      const temp = new Date();
+      temp.setDate(today.getDate() + i);
+      this.arr[i] = temp.toLocaleDateString('fr-CA');
+    }
   }
 
 }
